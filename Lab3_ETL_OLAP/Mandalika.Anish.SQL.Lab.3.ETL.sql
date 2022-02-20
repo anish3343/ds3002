0d42cb4fed6e2f2002b82fdd5e8672eb76721bb3
anish3343 <43268896+anish3343@users.noreply.github.com>
2022-02-19 22:30:25 -0500

    Lab 3 Part 2

--- Lab3_ETL_OLAP/Mandalika.Anish.SQL.Lab.3.ETL.sql
+++ Lab3_ETL_OLAP/Mandalika.Anish.SQL.Lab.3.ETL.sql
@@ -1,7 +1,15 @@
-# DROP DATABASE `northwind_dw`;
+# Anish Mandalika, am8wk@virginia.edu
+# DS 3002: Data Science Systems
+# Lab 3: ETL and OLAP
+
+#-------------------------------------------------------------------------------------------
+# PART 1: CREATE DIMENSIONAL SCHEMA
+#-------------------------------------------------------------------------------------------
+
+DROP DATABASE IF EXISTS `northwind_dw`;
 CREATE DATABASE `northwind_dw` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-DROP TABLE `dim_customers`;
+DROP TABLE IF EXISTS `dim_customers`;
 CREATE TABLE `dim_customers` (
   `customer_key` int NOT NULL AUTO_INCREMENT,
   `company` varchar(50) DEFAULT NULL,
@@ -30,7 +38,7 @@ CREATE TABLE `dim_customers` (
   KEY `state_province` (`state_province`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE `dim_employees`;
+DROP TABLE IF EXISTS `dim_employees`;
 CREATE TABLE `dim_employees` (
   `employee_key` int NOT NULL AUTO_INCREMENT,
   `company` varchar(50) DEFAULT NULL,
@@ -59,7 +67,7 @@ CREATE TABLE `dim_employees` (
   KEY `state_province` (`state_province`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE `dim_products`;
+DROP TABLE IF EXISTS `dim_products`;
 CREATE TABLE `dim_products` (
   # `supplier_ids` longtext,
   `product_key` int NOT NULL AUTO_INCREMENT,
@@ -79,7 +87,7 @@ CREATE TABLE `dim_products` (
   KEY `product_code` (`product_code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE `dim_shippers`;
+DROP TABLE IF EXISTS `dim_shippers`;
 CREATE TABLE `dim_shippers` (
   `shipper_key` int NOT NULL AUTO_INCREMENT,
   `company` varchar(50) DEFAULT NULL,
@@ -108,13 +116,13 @@ CREATE TABLE `dim_shippers` (
   KEY `state_province` (`state_province`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE `fact_orders`;
-CREATE TABLE `fact_orders`;
-
-CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `fact_orders`;
+CREATE TABLE `fact_orders`(
+  `fact_order_key` int NOT NULL AUTO_INCREMENT,
   `employee_id` int DEFAULT NULL,
   `customer_id` int DEFAULT NULL,
+  `order_id` int NOT NULL,
+  `product_id` int DEFAULT NULL,
   `order_date` datetime DEFAULT NULL,
   `shipped_date` datetime DEFAULT NULL,
   `shipper_id` int DEFAULT NULL,
@@ -128,67 +136,201 @@ CREATE TABLE `orders` (
   `taxes` decimal(19,4) DEFAULT '0.0000',
   `payment_type` varchar(50) DEFAULT NULL,
   `paid_date` datetime DEFAULT NULL,
-  `notes` longtext,
   `tax_rate` double DEFAULT '0',
   `tax_status_id` tinyint DEFAULT NULL,
-  `status_id` tinyint DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `customer_id` (`customer_id`),
-  KEY `customer_id_2` (`customer_id`),
-  KEY `employee_id` (`employee_id`),
-  KEY `employee_id_2` (`employee_id`),
-  KEY `id` (`id`),
-  KEY `id_2` (`id`),
-  KEY `shipper_id` (`shipper_id`),
-  KEY `shipper_id_2` (`shipper_id`),
-  KEY `id_3` (`id`),
-  KEY `tax_status` (`tax_status_id`),
-  KEY `ship_zip_postal_code` (`ship_zip_postal_code`),
-  KEY `fk_orders_orders_status1` (`status_id`),
-  CONSTRAINT `fk_orders_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
-  CONSTRAINT `fk_orders_employees1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
-  CONSTRAINT `fk_orders_orders_status1` FOREIGN KEY (`status_id`) REFERENCES `orders_status` (`id`),
-  CONSTRAINT `fk_orders_orders_tax_status1` FOREIGN KEY (`tax_status_id`) REFERENCES `orders_tax_status` (`id`),
-  CONSTRAINT `fk_orders_shippers1` FOREIGN KEY (`shipper_id`) REFERENCES `shippers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb3;
-
-CREATE TABLE `orders_status` (
-  `id` tinyint NOT NULL,
-  `status_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-CREATE TABLE `order_details` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `order_id` int NOT NULL,
-  `product_id` int DEFAULT NULL,
-  `quantity` decimal(18,4) NOT NULL DEFAULT '0.0000',
+  `order_status_name` varchar(50) NOT NULL,
+  `detail_id` int DEFAULT 0,
+  `quantity` decimal(18,4) DEFAULT '0.0000',
   `unit_price` decimal(19,4) DEFAULT '0.0000',
-  `discount` double NOT NULL DEFAULT '0',
-  `status_id` int DEFAULT NULL,
+  `discount` double DEFAULT '0',
+  `details_status_name` varchar(50),
   `date_allocated` datetime DEFAULT NULL,
   `purchase_order_id` int DEFAULT NULL,
   `inventory_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`),
-  KEY `inventory_id` (`inventory_id`),
-  KEY `id_2` (`id`),
-  KEY `id_3` (`id`),
-  KEY `id_4` (`id`),
-  KEY `product_id` (`product_id`),
-  KEY `product_id_2` (`product_id`),
-  KEY `purchase_order_id` (`purchase_order_id`),
-  KEY `id_5` (`id`),
-  KEY `fk_order_details_orders1_idx` (`order_id`),
-  KEY `fk_order_details_order_details_status1_idx` (`status_id`),
-  CONSTRAINT `fk_order_details_order_details_status1` FOREIGN KEY (`status_id`) REFERENCES `order_details_status` (`id`),
-  CONSTRAINT `fk_order_details_orders1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  CONSTRAINT `fk_order_details_products1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb3;
+  PRIMARY KEY (`fact_order_key`)
+)ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `order_details_status` (
-  `id` int NOT NULL,
-  `status_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+#-------------------------------------------------------------------------------------------
+# PART 2: POPULATE DIMENSIONAL SCHEMA
+#-------------------------------------------------------------------------------------------
+
+INSERT INTO `northwind_dw`.`dim_customers`
+(`customer_key`,
+`company`,
+`last_name`,
+`first_name`,
+`email_address`,
+`job_title`,
+`business_phone`,
+`home_phone`,
+`mobile_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`)
+SELECT `customers`.`id`,
+    `customers`.`company`,
+    `customers`.`last_name`,
+    `customers`.`first_name`,
+    `customers`.`email_address`,
+    `customers`.`job_title`,
+    `customers`.`business_phone`,
+    `customers`.`home_phone`,
+    `customers`.`mobile_phone`,
+    `customers`.`fax_number`,
+    `customers`.`address`,
+    `customers`.`city`,
+    `customers`.`state_province`,
+    `customers`.`zip_postal_code`,
+    `customers`.`country_region`
+FROM `northwind`.`customers`;
+
+INSERT INTO `northwind_dw`.`dim_employees`
+(`employee_key`,
+`company`,
+`last_name`,
+`first_name`,
+`email_address`,
+`job_title`,
+`business_phone`,
+`home_phone`,
+`mobile_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`,
+`web_page`)
+SELECT `employees`.`id`,
+    `employees`.`company`,
+    `employees`.`last_name`,
+    `employees`.`first_name`,
+    `employees`.`email_address`,
+    `employees`.`job_title`,
+    `employees`.`business_phone`,
+    `employees`.`home_phone`,
+    `employees`.`mobile_phone`,
+    `employees`.`fax_number`,
+    `employees`.`address`,
+    `employees`.`city`,
+    `employees`.`state_province`,
+    `employees`.`zip_postal_code`,
+    `employees`.`country_region`,
+    `employees`.`web_page`
+FROM `northwind`.`employees`;
+
+INSERT INTO `northwind_dw`.`dim_products`
+(`product_key`,
+`product_code`,
+`product_name`,
+`standard_cost`,
+`list_price`,
+`reorder_level`,
+`target_level`,
+`quantity_per_unit`,
+`discontinued`,
+`minimum_reorder_quantity`,
+`category`)
+SELECT `products`.`id`,
+    `products`.`product_code`,
+    `products`.`product_name`,
+    `products`.`standard_cost`,
+    `products`.`list_price`,
+    `products`.`reorder_level`,
+    `products`.`target_level`,
+    `products`.`quantity_per_unit`,
+    `products`.`discontinued`,
+    `products`.`minimum_reorder_quantity`,
+    `products`.`category`
+FROM `northwind`.`products`;
+
+INSERT INTO `northwind_dw`.`dim_shippers`
+(`shipper_key`,
+`company`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`)
+SELECT `shippers`.`id`,
+    `shippers`.`company`,
+    `shippers`.`address`,
+    `shippers`.`city`,
+    `shippers`.`state_province`,
+    `shippers`.`zip_postal_code`,
+    `shippers`.`country_region`
+FROM `northwind`.`shippers`;
+
+INSERT INTO `northwind_dw`.`fact_orders`
+(`employee_id`,
+`customer_id`,
+`order_id`,
+`product_id`,
+`order_date`,
+`shipped_date`,
+`shipper_id`,
+`ship_name`,
+`ship_address`,
+`ship_city`,
+`ship_state_province`,
+`ship_zip_postal_code`,
+`ship_country_region`,
+`shipping_fee`,
+`taxes`,
+`payment_type`,
+`paid_date`,
+`tax_rate`,
+`tax_status_id`,
+`order_status_name`,
+`detail_id`,
+`quantity`,
+`unit_price`,
+`discount`,
+`details_status_name`,
+`date_allocated`,
+`purchase_order_id`,
+`inventory_id`)
+SELECT `orders`.`employee_id`,
+    `orders`.`customer_id`,
+    `orders`.`id`,
+    `order_details`.`product_id`,
+    `orders`.`order_date`,
+    `orders`.`shipped_date`,
+    `orders`.`shipper_id`,
+    `orders`.`ship_name`,
+    `orders`.`ship_address`,
+    `orders`.`ship_city`,
+    `orders`.`ship_state_province`,
+    `orders`.`ship_zip_postal_code`,
+    `orders`.`ship_country_region`,
+    `orders`.`shipping_fee`,
+    `orders`.`taxes`,
+    `orders`.`payment_type`,
+    `orders`.`paid_date`,
+    `orders`.`tax_rate`,
+    `orders`.`tax_status_id`,
+    `orders_status`.`status_name`,
+    `order_details`.`id`,
+    `order_details`.`quantity`,
+    `order_details`.`unit_price`,
+    `order_details`.`discount`,
+    `order_details_status`.`status_name`,
+    `order_details`.`date_allocated`,
+    `order_details`.`purchase_order_id`,
+    `order_details`.`inventory_id`    
+FROM `northwind`.`orders`
+LEFT JOIN `northwind`.`orders_status`
+ON `northwind`.`orders_status`.`id` = `northwind`.`orders`.`status_id`
+LEFT JOIN `northwind`.`order_details`
+ON `northwind`.`orders`.`id` = `northwind`.`order_details`.`order_id`
+LEFT JOIN `northwind`.`order_details_status`
+ON `northwind`.`order_details`.`status_id` = `northwind`.`order_details_status`.`id`;
+
+#-------------------------------------------------------------------------------------------
+# PART 3: DIMENSIONAL REPORT
+#-------------------------------------------------------------------------------------------
 
